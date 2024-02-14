cf56aafee9edf2d85e647abbbec3d2954c1e9fe0
gnemek20 <gnemek20@gmail.com>
2024-02-14 17:18:33 +0900

    타이틀 수정 및 input 확대 안 되게 meta 수정, 모바일 확인을 위한 몇몇 요소 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>DY</title>
+        <meta name="viewport" content="user-scalable=no" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
