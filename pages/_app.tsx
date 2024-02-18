c8f74ada282f0a0c3fc040721430b7c202f01d91
gnemek20 <gnemek20@gmail.com>
2024-02-18 19:07:40 +0900

    Script Head 바깥에 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,12 +9,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>DY</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-        <Script
-          type="text/javascript"
-          strategy="beforeInteractive"
-          src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=6kbpgfbf8d"
-        ></Script>
       </Head>
+      <Script
+        type="text/javascript"
+        strategy="beforeInteractive"
+        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=6kbpgfbf8d"
+      ></Script>
       <Component {...pageProps} />
     </div>
   )
