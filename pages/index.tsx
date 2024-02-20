0ba8180c0dfeb2e09616a3ba39f949983d31d458
gnemek20 <gnemek20@gmail.com>
2024-02-20 22:09:43 +0900

    banner의 height 모바일에서 --vh로 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -207,15 +207,15 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    // map_ref.current = new naver.maps.Map('map', {
-    //   center: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
-    //   zoomControl: true,
-    //   zoom: 15
-    // });
-    // new naver.maps.Marker({
-    //   position: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
-    //   map: map_ref.current
-    // });
+    map_ref.current = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
+      zoomControl: true,
+      zoom: 15
+    });
+    new naver.maps.Marker({
+      position: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
+      map: map_ref.current
+    });
   }, [ is_mobile ]);
 
   return (
