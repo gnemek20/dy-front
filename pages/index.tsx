03b80a889e0b873eec00554e9d6ef637bdcf3a49
gnemek20 <gnemek20@gmail.com>
2024-02-28 02:33:46 +0900

    지도 주석 해제

--- pages/index.tsx
+++ pages/index.tsx
@@ -255,15 +255,15 @@ const Home = (serverSideProps: InferGetServerSidePropsType<typeof getServerSideP
   }, []);
 
   useEffect(() => {
-    // map_ref.current = new naver.maps.Map('map', {
-    //   center: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
-    //   zoomControl: true,
-    //   zoom: 15
-    // });
-    // new naver.maps.Marker({
-    //   position: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
-    //   map: map_ref.current
-    // });
+    map_ref.current = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
+      zoomControl: true,
+      zoom: 15
+    });
+    new naver.maps.Marker({
+      position: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
+      map: map_ref.current
+    });
   }, [ is_mobile ]);
 
   return (
